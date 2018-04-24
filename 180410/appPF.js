4d0ce960869e85130d7c6a6f42d0dac4b5094264
ohmijin <mijinoh8745@gmail.com>
2018-04-24 15:14:38 +0900

    commit succeeded~!

--- 180410/appPF.js
+++ 180410/appPF.js
@@ -11,63 +11,58 @@ var connection = mysql.createConnection({
   database: 'test'
 });
 
-var server = http.createServer(app).listen(800);
+var server = http.createServer(app).listen(80);
 connection.connect();
 console.log("hello nodejs");
-//
+
 app.get('/test', function(req, res) {
-  res.send('hello ohmijin');
+  res.send('hello 조준형112223333');
 });
-//
-connection.query('select * from member2',
-  function(err, rows, fields) {
-    if (err) throw err;
-    console.log(rows);
-  });
 
 app.get('/cssPractice', function(req, res) {
-  res.sendfile('css1.html')
+  res.sendfile('css실습1.html');
 });
-//
+
 app.get('/getAllMember2', function(req, res) {
   connection.query('select * from member2',
     function(err, rows, fields) {
       if (err) throw err;
       res.send(rows);
-    });
+    }
+  );
 });
-//
+
 app.get('/getAuthcode1or2', function(req, res) {
   connection.query('select * from member where authcode=1 or authcode=2',
     function(err, rows, fields) {
       if (err) throw err;
       res.send(rows);
-    });
+    }
+  );
 });
-
 app.get('/getNameFromBoard', function(req, res) {
-  connection.query('select name from member where id'+
-  ' in (select id from board)',
+  connection.query('select name from member where id in (select id from board)',
     function(err, rows, fields) {
       if (err) throw err;
       res.send(rows);
-    });
+    }
+  );
 });
-
 app.get('/getEmailOfAdmin', function(req, res) {
-  connection.query('select email from member where authcode = '+
-  ' (select authcode from auth where authority="관리자")',
+  connection.query("select email from member where authcode = \
+	(select authcode from auth where authority='관리자')",
     function(err, rows, fields) {
       if (err) throw err;
       res.send(rows);
-    });
+    }
+  );
 });
-
 app.get('/getLongestEmail', function(req, res) {
-  connection.query('select * from member where length(email)' +
-  ' = (select max(length(email)) from member)',
+  connection.query("select * from member where length(email) = \
+	(select max(length(email)) from member)",
     function(err, rows, fields) {
       if (err) throw err;
       res.send(rows);
-    });
-})
+    }
+  );
+});
